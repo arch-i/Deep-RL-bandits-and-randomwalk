4885e57c08eaef6cef8ce78c8b4a6a28fe9073d2
arch-i <Archi.gupta2@gmail.com>
2021-09-11 09:58:10 +0530

    Create bandit_env.py

---
+++ custom_envs/my_envs/envs/bandit_env.py
@@ -0,0 +1,59 @@
+import gym 
+from gym import Env
+import numpy as np
+
+
+
+class bernoulliBandit(Env):
+  def __init__(self, alpha, beta, seed):
+    #actions 0 (left), 1(right)
+    self.action_space = [0,1]
+    self.observation_space=[0,1,2]
+    self.state=1
+    self.alpha = alpha
+    self.beta = beta
+    self.rng = np.random.default_rng(seed)
+  
+  def __str__(self):
+    return '2 armed Bernoulli Bandit \n Alpha:'+str(self.alpha)+ ' Beta:'+str(self.beta)
+
+  def step(self, action):
+    if action:
+      action = self.rng.binomial(1,self.beta)
+    else:
+      action = self.rng.binomial(1,1-self.alpha)
+    self.state += -1 + 2*action
+    reward = action
+    return reward
+
+  def render(self):
+    pass
+  def reset(self):
+    self.state=1
+    
+    
+    
+class gaussianBandit(Env):
+  def __init__(self,seed):
+    #actions 10 
+    self.action_space = np.arange(10)
+    self.observation_space=np.arange(11)
+    self.state=0
+    self.rng = np.random.default_rng(seed)
+    self.rewards = np.zeros(10)
+    for i in range(10):
+      self.rewards[i]= rng.normal(0,1,1)
+    
+  
+  def __str__(self):
+    return '10 armed Gaussian Bandit with rewards: ' + str(self.rewards)
+
+  def step(self, action):
+    reward = rng.normal(self.rewards[action],1,1)
+    self.state = action+1
+    return reward
+
+  def render(self):
+    pass
+  def reset(self):
+    self.state=0
