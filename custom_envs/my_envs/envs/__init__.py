6f971d8006d4d53a1cb69add8785c15cf6bfe0cc
arch-i <Archi.gupta2@gmail.com>
2021-09-11 11:06:23 +0530

    Update __init__.py

--- custom_envs/my_envs/envs/__init__.py
+++ custom_envs/my_envs/envs/__init__.py
@@ -1,3 +1,3 @@
 from my_envs.envs.bandit_env import bernoulliBandit
 from my_envs.envs.bandit_env import gaussianBandit
-from my_ens.envs.walk_env import randomWalk
+from my_envs.envs.walk_env import randomWalk
